724d88aba1273aac7d40a2ce15de6e86d1617bab
timbukflow <ivoschwizer@gmail.com>
2021-04-13 13:00:26 +0200

    fav

--- head.php
+++ head.php
@@ -12,9 +12,6 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7f7f7f">
-<meta name="msapplication-TileColor" content="#313131">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
 <meta property="og:title" content="Heidisatelier">
